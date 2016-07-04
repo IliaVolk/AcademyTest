9219082a6a2c3f4c3e1e0514d053546b71c8c5ea
Swites <liailia119@gmail.com>
2016-07-04 20:04:20 +0300

    5: Add onclick to button

--- index.js
+++ index.js
@@ -3,4 +3,12 @@
     var button = document.getElementById("button");
     var answer = document.getElementById("answer");
     answer.innerHTML = "no answer yet";
+    button.onclick = function(){
+        var number = input.value;
+        if (number % 2 === 0){
+            answer.innerHTML = "This number is odd";
+        }else {
+            answer.innerHTML = "This number is even";
+        }
+    }
 })();
